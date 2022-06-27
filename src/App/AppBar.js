56f8ddca2cbfde3a1bfa9b7f0d09afcaadfccdd9
Walker <walkerclaury@protonmail.com>
2022-06-27 18:22:20 -0500

    Styled app bar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,16 +1,37 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+
+const Logo = styled.div`
+    font-size: 1.5em;
+
+`
+const ControlButtonElem = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        text-shadow: 0 0 60px #03ff03;
+    `}
+`
+function toProperCase(lower) {
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
 
 const Bar = styled.div`
 display: grid;
+margin-bottom: 40px;
 grid-template-columns: 180px auto 100px 100px;
 `
+function ControlButton({ name, active }) {
+    return (<ControlButtonElem active={active}>
+        {toProperCase(name)}
+    </ControlButtonElem>)
+}
+
 export default function AppBar() {
     return (
         <Bar>
-            <div>CryptoDash</div>
+            <Logo>CryptoDash</Logo>
             <div></div>
-            <div>Dashboard</div>
-            <div>Settings</div>
+            <ControlButton active name='dashboard' />
+            <ControlButton name='settings' />
         </Bar>
     )
 }
