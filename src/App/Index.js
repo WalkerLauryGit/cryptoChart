70fa78af8271a784e9828ee3c99d6e6386e8ef8e
Walker <walkerclaury@protonmail.com>
2022-06-27 18:10:34 -0500

    Added CSS grid in app bar

--- src/App/Index.js
+++ src/App/Index.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AppLayout from './AppLayout';
 import Welcome from './Welcome';
+import AppBar from './AppBar';
 function Index() {
   return (
     <AppLayout >
+      <AppBar/>
       <Welcome/>
     </AppLayout>
   );
